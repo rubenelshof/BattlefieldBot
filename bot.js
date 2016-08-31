fd62b37312f7627855744f75cf03da4abac258c3
RE1992 <re161992@gmail.com>
2016-09-01 01:03:30 +0200

    Add BFBC and BF3 Stats. Removed jokes

--- bot.js
+++ bot.js
@@ -63,6 +63,56 @@ bot.on("message", function(message) {
   }
 });
 
+//bfbc2stats
+bot.on("message", function(message) {
+  if (!message.channel.isPrivate) {
+    //get content
+    var content = message.content;
+    var name = content.split(" ");
+    var playername = name[1];
+
+    //api
+    var bfbc2statsapi = "http://api.bfbcs.com/api/pc?players=" + playername + "&fields=all";
+
+    //url
+    var bfbc2stats = "http://bfbcs.com/";
+    var url = bfbc2stats + playername;
+
+    if (message.content === ("!bfbc2rank " + playername)) {getJSON(bfbc2statsapi, function(err, res, callback) {bot.sendMessage(message, "Your rank is **" + (res["players"][0]["rank"]) + "**");});}
+    if (message.content === ("!bfbc2skill " + playername)) {getJSON(bfbc2statsapi, function(err, res, callback) {bot.sendMessage(message, "Your skill score is **" + (res["players"][0]["elo"]).toFixed(0) + "**");});}
+    if (message.content === ("!bfbc2kd " + playername)) {getJSON(bfbc2statsapi, function(err, res, callback) { var kills = (res["players"][0]["kills"]); var deaths = (res["players"][0]["deaths"]); bot.sendMessage(message, "Your K/D is **" + (kills / deaths).toFixed(2) + "**");});}
+  } else {
+    if (message.content === "!stats") {
+      bot.sendMessage(message, "Hello, Your on **PM**");
+    }
+  }
+});
+
+//bf3stats
+bot.on("message", function(message) {
+  if (!message.channel.isPrivate) {
+    //get content
+    var content = message.content;
+    var name = content.split(" ");
+    var playername = name[1];
+
+    //api
+    var bf3statsapi = "http://api.bf3stats.com/pc/player/player=" + playername + "&opt=all";
+
+    //url
+    var bf3stats = "http://bf3stats.com/pc/";
+    var url = bf3stats + playername;
+
+    if (message.content === ("!bf3rank " + playername)) {getJSON(bf3statsapi, function(err, res, callback) {bot.sendMessage(message, "Your rank is **" + (res.stats.rank.nr) + "**");});}
+    if (message.content === ("!bf3skill " + playername)) {getJSON(bf3statsapi, function(err, res, callback) {bot.sendMessage(message, "Your skill score is **" + (res.stats.global.elo).toFixed(0) + "**");});}
+    if (message.content === ("!bf3kd " + playername)) {getJSON(bf3statsapi, function(err, res, callback) {bot.sendMessage(message, "Your K/D is **" + (res.stats.global.elo_games).toFixed(0) + "**");});}
+  } else {
+    if (message.content === "!stats") {
+      bot.sendMessage(message, "Hello, Your on **PM**");
+    }
+  }
+});
+
 //bf4stats
 bot.on("message", function(message) {
   if (!message.channel.isPrivate) {
@@ -112,24 +162,7 @@ bot.on("message", function(message) {
 
 //user command. create by admin
 
-// jokes
-//var flickrphotos = "http://api.flickr.com/services/feeds/photos_public.gne?jsoncallback=?"
-//cats
-//bot.on("message", function(message) {
-//  if (!message.channel.isPrivate) {
 
-//    var catkeyword = "cats, cat, cute cat";
-
-//    if (message.content === ("!cats ")) {getJSON(flickrphotos,{tags: catkeyword, tagmode: "any", format: "json"}, function(err, res, callback) {bot.sendMessage(message, "Your rank is **" + (res.stats.rank) + "**");});}
-//  }
-//});
-
-//yomomma
-//bot.on("message", function(message) {
-//  if (!message.channel.isPrivate) {
-//    if (message.content === ("!yomomma")) {getJSON("http://api.yomomma.info/", function(err, res, callback) {bot.sendMessage(message, (res.joke));});}
-//  }
-//});
 
 //token
 var token = require('./token.js');
