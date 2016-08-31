893ca49b22507244410173e44e7a8be5c9d85c0a
RE1992 <re161992@gmail.com>
2016-08-31 18:07:58 +0200

    Comment out unnecessary stuff

--- bot.js
+++ bot.js
@@ -2,16 +2,25 @@ var Discord = require("discord.js");
 const mysql = require('mysql');
 var bot = new Discord.Client();
 
+//required modules
+const http = require('http');
+const request = require('request');
+const getJSON = require('get-json');
+var parseString = require('xml2js').parseString;
+
 //debug
 const util = require('util')
 console.log(util.inspect(Discord, false, null))
 
+//db
 var db = require('./db.js');
 
+//console log
 bot.on("ready", function() {
   console.log("Bot online and ready!");
 });
 
+//dbservercreate
 bot.on("serverCreated", function(server) {
   console.log("Trying to insert server " + server.name + " into database");
   var info = {
@@ -30,6 +39,7 @@ bot.on("serverCreated", function(server) {
   })
 });
 
+//dbserverdelete
 bot.on("serverDeleted", function(server) {
   console.log("Trying to remove server " + server.name + " from database");
   connection.query("DELETE FROM servers WHERE serverid = '" + server.id + "'", function(error) {
@@ -41,7 +51,7 @@ bot.on("serverDeleted", function(server) {
   })
 })
 
-//server
+//currentserver
 bot.on("message", function(message) {
   if (!message.channel.isPrivate) {
     if (message.content === "!server") {
@@ -54,11 +64,7 @@ bot.on("message", function(message) {
   }
 });
 
-//bfstats
-const http = require('http');
-const request = require('request');
-const getJSON = require('get-json');
-
+//bf4stats
 bot.on("message", function(message) {
   if (!message.channel.isPrivate) {
     //get content
@@ -73,9 +79,9 @@ bot.on("message", function(message) {
     var bf4stats = "http://bf4stats.com/pc/";
     var url = bf4stats + playername;
 
-    if (message.content === ("!rank " + playername)) {getJSON(bf4statsapi, function(err, res, callback) {bot.sendMessage(message, "Your rank is **" + (res.stats.rank) + "**");});}
-    if (message.content === ("!skill " + playername)) {getJSON(bf4statsapi, function(err, res, callback) {bot.sendMessage(message, "Your skill score is **" + (res.stats.skill) + "**");});}
-    if (message.content === ("!kd " + playername)) {getJSON(bf4statsapi, function(err, res, callback) {bot.sendMessage(message, "Your K/D is **" + (res.stats.extra.kdr).toFixed(2) + "**");});}
+    if (message.content === ("!bf4rank " + playername)) {getJSON(bf4statsapi, function(err, res, callback) {bot.sendMessage(message, "Your rank is **" + (res.stats.rank) + "**");});}
+    if (message.content === ("!bf4skill " + playername)) {getJSON(bf4statsapi, function(err, res, callback) {bot.sendMessage(message, "Your skill score is **" + (res.stats.skill) + "**");});}
+    if (message.content === ("!bf4kd " + playername)) {getJSON(bf4statsapi, function(err, res, callback) {bot.sendMessage(message, "Your K/D is **" + (res.stats.extra.kdr).toFixed(2) + "**");});}
   } else {
     if (message.content === "!stats") {
       bot.sendMessage(message, "Hello, Your on **PM**");
@@ -83,15 +89,48 @@ bot.on("message", function(message) {
   }
 });
 
+//overwatchstats
+bot.on("message", function(message) {
+  if (!message.channel.isPrivate) {
+    //get content
+    var content = message.content;
+    var name = content.split(" ");
+    var playername = name[1];
+
+    //api
+    var divisionapi = "https://2g.be/twitch/Division/command/action=" + playername + "?plat=pc&name=" + playername + "&output=json";
+
+    //url
+    var bf4stats = "http://bf4stats.com/pc/";
+    var url = bf4stats + playername;
+
+    if (message.content === ("!rank " + playername)) {getJSON(bf4statsapi, function(err, res, callback) {bot.sendMessage(message, "Your rank is **" + (res.stats.rank) + "**");});}
+    if (message.content === ("!skill " + playername)) {getJSON(bf4statsapi, function(err, res, callback) {bot.sendMessage(message, "Your skill score is **" + (res.stats.skill) + "**");});}
+    if (message.content === ("!kd " + playername)) {getJSON(bf4statsapi, function(err, res, callback) {bot.sendMessage(message, "Your K/D is **" + (res.stats.extra.kdr).toFixed(2) + "**");});}
+  }
+});
+
+
 //user command. create by admin
 
 // jokes
+//var flickrphotos = "http://api.flickr.com/services/feeds/photos_public.gne?jsoncallback=?"
+//cats
+//bot.on("message", function(message) {
+//  if (!message.channel.isPrivate) {
+
+//    var catkeyword = "cats, cat, cute cat";
+
+//    if (message.content === ("!cats ")) {getJSON(flickrphotos,{tags: catkeyword, tagmode: "any", format: "json"}, function(err, res, callback) {bot.sendMessage(message, "Your rank is **" + (res.stats.rank) + "**");});}
+//  }
+//});
+
 //yomomma
-bot.on("message", function(message) {
-  if (!message.channel.isPrivate) {
-    if (message.content === ("!yomomma")) {getJSON("http://api.yomomma.info/", function(err, res, callback) {bot.sendMessage(message, (res.joke));});}
-  }
-});
+//bot.on("message", function(message) {
+//  if (!message.channel.isPrivate) {
+//    if (message.content === ("!yomomma")) {getJSON("http://api.yomomma.info/", function(err, res, callback) {bot.sendMessage(message, (res.joke));});}
+//  }
+//});
 
 //token
 var token = require('./token.js');
